d3e4207d3583d37adaf538dbcc7302983dbefb03
sercanno <Sergio.cannobbio@inacapmail.cl>
2024-04-10 20:02:41 -0400

    modified Risto/setting/PORT

--- Risto/settings.py
+++ Risto/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = 'Risto.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/Risto',
+        default='postgresql://postgres:postgres@localhost:10000/RISTORENDER',
         conn_max_age=600    
     )
 }
