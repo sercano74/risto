162fd40d7db3e6e78104d9f0f955c40fa158ffcb
sercanno <Sergio.cannobbio@inacapmail.cl>
2024-04-15 23:18:33 -0400

    hsst SECOND

--- Risto/settings.py
+++ Risto/settings.py
@@ -32,7 +32,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
-DEBUG = 'RENDER' not in os.environ
+# DEBUG = 'RENDER' not in os.environ
+DEBUG = os.environ.get('DEBUG')
 
 ALLOWED_HOSTS = []
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
@@ -162,4 +163,4 @@ DEFAULT_AUTO_FIELD='django.db.models.BigAutoField'
 SESSION_COOKIE_SECURE=True
 CSRF_COOKIE_SECURE=True
 SECURE_SSL_REDIRECT=True
-
+SECURE_HSTS_SECONDS=31536000
