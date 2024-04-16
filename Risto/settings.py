3296b3917846067c0ca553ad02fee039340ebd6e
sercanno <Sergio.cannobbio@inacapmail.cl>
2024-04-15 22:02:48 -0400

    RistoDB

--- Risto/settings.py
+++ Risto/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'Risto.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/RistoDB',
         conn_max_age=600
     )
 }
